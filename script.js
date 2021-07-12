50f7280c8ed8b8dd4d7eaef4181ed04c1b0fed1a
“daniel1954123” <m.danik20.07@gmail.com>
2021-07-13 01:40:49 +0600

    Added home-work-12

---
+++ script.js
@@ -0,0 +1,48 @@
+'use strict'
+
+// let a = 1;
+// do {
+//     console.log('Angry Birds', a);
+//     a++
+// }while (a<=100)
+
+
+// for (let a = 11; a <= 33; a++){
+//     console.log(a);
+// }
+
+// const arr =  [1, 2, 3, 4, 5];
+//
+// for (let a = 0; a < arr.length; a++){
+//     console.log(arr[a]);
+// }
+
+// const obj = {green: 'зеленый', red: 'красный', blue: 'голубой'}
+// for (const prop in obj){
+//     console.log('obj.'+ prop + "=" + obj[prop]);
+// }
+
+// const obj = {Коля:'Зарплата 200 Долларов',Вася:'Зарплата 300 Долларов',Петя:'Зарплата 400 Долларов'}
+// for (const prop in obj){
+//     console.log(prop + '-' + obj[prop]);
+// }
+
+// const arr = [2, 5, 9, 15, 0, 4];
+// for (let a = 0; a<arr.length; a++){
+//     if (arr[a] === 5 || arr[a] ===9 || arr[a] ===4){
+//         console.log(arr[a])
+//     }
+// }
+
+// const weeksArr = ['Monday', 'Tuesday','Wednesday', 'Thursday', 'Friday', 'Sunday', 'Saturday'];
+// for (let a = 0; a<weeksArr.length; a++){
+//     console.log(weeksArr[a]);
+// }
+
+
+
+
+
+
+
+
